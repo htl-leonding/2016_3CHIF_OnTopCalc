cf350d867865b119bafbacb391379b9bca780fcd
Schloemicher <a.schloemi@gmail.com>
2016-03-21 16:52:47 +0100

    Added Buttons in "AllProjectView"

--- ApplicationGuiMainForm/src/main/java/controller/AllProjectsController.java
+++ ApplicationGuiMainForm/src/main/java/controller/AllProjectsController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import com.sun.prism.impl.shape.OpenPiscesPrismUtils;
 import db.controller.ProjectController;
 import entity.Client;
 import entity.Project;
@@ -7,11 +8,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -36,6 +45,8 @@ public class AllProjectsController implements Initializable {
     private TableColumn<Project, Client> tc_Client;
     
     private ObservableList<Project> projects;
+    @FXML
+    private TableColumn tc_action;
 
     /**
      * Initializes the controller class. Adds all projects from the database
@@ -62,5 +73,73 @@ public class AllProjectsController implements Initializable {
                 ProjectViewController.getInstance().openProject(tv_ProjectList.getSelectionModel().getSelectedItem());
             }
         });
+        
+        tc_action.setCellValueFactory( new PropertyValueFactory<>( "DUMMY" ) );
+
+        Callback<TableColumn<Project, String>, TableCell<Project, String>> cellFactory = //
+                new Callback<TableColumn<Project, String>, TableCell<Project, String>>()
+                {
+                    @Override
+                    public TableCell call( final TableColumn<Project, String> param )
+                    {
+                        final TableCell<Project, String> cell = new TableCell<Project, String>()
+                        {
+
+                            final Label openP = new Label();
+                            final Label printP = new Label();
+                            final Label costingP = new Label();
+                            final Label copyP = new Label();
+                            final Label deleteP = new Label();
+                            final HBox box = new HBox(openP,printP,costingP,copyP,deleteP);
+                            
+                            @Override
+                            public void updateItem( String item, boolean empty )
+                            {
+                                super.updateItem( item, empty );
+                                if ( empty )
+                                {
+                                    setGraphic( null );
+                                    setText( null );
+                                }
+                                else
+                                {
+                                    openP.setId("edit");
+                                    printP.setId("print");
+                                    costingP.setId("costing");
+                                    copyP.setId("copy");
+                                    deleteP.setId("delete");
+                                    box.setId("box");
+                                    
+                                    openP.setOnMouseClicked((MouseEvent event) -> {
+                                        Project p = getTableView().getItems().get( getIndex() );
+                                        //TODO
+                                    });
+                                    printP.setOnMouseClicked((MouseEvent event) -> {
+                                        Project p = getTableView().getItems().get( getIndex() );
+                                        //TODO
+                                    });
+                                    costingP.setOnMouseClicked((MouseEvent event) -> {
+                                        Project p = getTableView().getItems().get( getIndex() );
+                                        //TODO
+                                    });
+                                    copyP.setOnMouseClicked((MouseEvent event) -> {
+                                        Project p = getTableView().getItems().get( getIndex() );
+                                        //TODO
+                                    });
+                                    deleteP.setOnMouseClicked((MouseEvent event) -> {
+                                        Project p = getTableView().getItems().get( getIndex() );
+                                        //TODO
+                                    });
+                                    setGraphic(box);
+                                    setText( null );
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+
+        tc_action.setCellFactory( cellFactory );
+        
     }    
 }
