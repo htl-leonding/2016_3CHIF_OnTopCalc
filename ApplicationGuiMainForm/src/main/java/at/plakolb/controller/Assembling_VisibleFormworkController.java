0154040e955ff0794b2c5e9d42d5b6dda09ae886
Schloemicher <a.schloemi@gmail.com>
2016-04-14 19:54:38 +0200

    Sichtbare Schalung
    #12

--- ApplicationGuiMainForm/src/main/java/at/plakolb/controller/Assembling_VisibleFormworkController.java
+++ ApplicationGuiMainForm/src/main/java/at/plakolb/controller/Assembling_VisibleFormworkController.java
@@ -1,8 +1,12 @@
 package at.plakolb.controller;
 
+import at.plakolb.calculationlogic.db.controller.CategoryController;
+import at.plakolb.calculationlogic.db.controller.ComponentController;
 import at.plakolb.calculationlogic.db.controller.ParameterController;
 import at.plakolb.calculationlogic.db.controller.ProductController;
 import at.plakolb.calculationlogic.db.controller.WorthController;
+import at.plakolb.calculationlogic.entity.Category;
+import at.plakolb.calculationlogic.entity.Component;
 import at.plakolb.calculationlogic.eunmeration.ProductType;
 import at.plakolb.calculationlogic.entity.Product;
 import at.plakolb.calculationlogic.entity.Project;
@@ -12,6 +16,8 @@ import java.net.URL;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -52,10 +58,11 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
     @FXML
     private Label lb_RoofArea;
 
-    private Worth pricePerSquare;
+    private double pricePerSquare;
     private Worth abatementPercent;
     private Worth workerCosts;
     private Worth assemblingDuration;
+
     private Worth abatementArea;
     private Worth visibleFormwork;
     private Worth productCosts;
@@ -79,12 +86,10 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
         abatementArea = new Worth(parameterController.findParameterPByShortTerm("VSS"));
         visibleFormwork = new Worth(parameterController.findParameterPByShortTerm("SS"));
         productCosts = new Worth(parameterController.findParameterPByShortTerm("SSPROK"));
-        pricePerSquare = new Worth(parameterController.findParameterPByShortTerm("KPSS"));
         assemblingDuration = new Worth(parameterController.findParameterPByShortTerm("ZPSS"));
         assemblingCosts = new Worth(parameterController.findParameterPByShortTerm("KMSS"));
         totalCosts = new Worth(parameterController.findParameterPByShortTerm("GKSS"));
-        //workerCosts = new Worth(parameterController.findParameterPByShortTerm(""));
-        workerCosts = new Worth();
+        workerCosts = new Worth(parameterController.findParameterPByShortTerm("KPSS"));
 
         tf_AbatementPercent.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
             setAbatementPercent();
@@ -106,6 +111,10 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
             calculateFormwork();
         });
 
+        cb_Product.getSelectionModel().selectedItemProperty().addListener((source, oldValue, newValue) -> {
+            tf_PricePerSquare.setText(UtilityFormat.getStringForTextField(newValue.getPriceUnit()));
+        });
+
         if (ProjectViewController.getOpenedProject() != null) {
             loadValuesFromDataBase();
         }
@@ -116,8 +125,8 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
     }
 
     public void setPricePerSquare() {
-        pricePerSquare.setWorth(tf_PricePerSquare.getText().isEmpty() || !tf_PricePerSquare.getText().matches("[0-9]*.[0-9]*")
-                ? 0 : Double.valueOf(tf_PricePerSquare.getText().replace(',', '.')));
+        pricePerSquare = tf_PricePerSquare.getText().isEmpty() || !tf_PricePerSquare.getText().matches("[0-9]*.[0-9]*")
+                ? 0 : Double.valueOf(tf_PricePerSquare.getText().replace(',', '.'));
     }
 
     public void setAbatementPercent() {
@@ -140,11 +149,20 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
         WorthController worthController = new WorthController();
         Project openedProject = ProjectViewController.getOpenedProject();
 
+        Category category = new CategoryController().findCategoryByShortTerm("SS");
+        if (category != null) {
+            Component component = new ComponentController().findComponentByProjectIdAndComponentTypeAndCategoryId(openedProject.getId(), "Produkt", category.getId());
+            if (component != null) {
+                cb_Product.getSelectionModel().select(component.getProduct());
+                tf_PricePerSquare.setText(UtilityFormat.worthWithTwoDecimalPlaces(component.getPriceComponent()));
+            }
+        }
+
         abatementPercent = (worthController.findWorthByShortTermAndProjectId("VSSP", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("VSSP", openedProject.getId()) : abatementPercent;
         abatementArea = (worthController.findWorthByShortTermAndProjectId("VSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("VSS", openedProject.getId()) : abatementArea;
         visibleFormwork = (worthController.findWorthByShortTermAndProjectId("SS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("SS", openedProject.getId()) : visibleFormwork;
         productCosts = (worthController.findWorthByShortTermAndProjectId("SSPROK", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("SSPROK", openedProject.getId()) : productCosts;
-        pricePerSquare = (worthController.findWorthByShortTermAndProjectId("KPSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("KPSS", openedProject.getId()) : pricePerSquare;
+        workerCosts = (worthController.findWorthByShortTermAndProjectId("KPSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("KPSS", openedProject.getId()) : workerCosts;
         assemblingDuration = (worthController.findWorthByShortTermAndProjectId("ZPSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("ZPSS", openedProject.getId()) : assemblingDuration;
         assemblingCosts = (worthController.findWorthByShortTermAndProjectId("KMSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("KMSS", openedProject.getId()) : assemblingCosts;
         totalCosts = (worthController.findWorthByShortTermAndProjectId("GKSS", openedProject.getId()) != null) ? worthController.findWorthByShortTermAndProjectId("GKSS", openedProject.getId()) : totalCosts;
@@ -162,19 +180,43 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
 
     public void persistVisibleFormwork() {
         WorthController worthController = new WorthController();
+        
+        Category category = new CategoryController().findCategoryByShortTerm("SS");
+        if (category != null) {
+            try {
+                ComponentController componentController = new ComponentController();
+                Product product = cb_Product.getSelectionModel().getSelectedItem();
+                Component component = componentController.findComponentByProjectIdAndComponentTypeAndCategoryId(ProjectViewController.getOpenedProject().getId(), "SS", category.getId());
+                if (component == null) {
+                    component = new Component();
+                }
+                component.setDescription(product.getName());
+                component.setCategory(category);
+                component.setComponentType("Produkt");
+                component.setLengthComponent(product.getLengthProduct());
+                component.setWidthComponent(product.getWidthProduct());
+                component.setHeightComponent(product.getHeightProduct());
+                component.setProduct(product);
+                component.setProject(ProjectViewController.getOpenedProject());
+                component.setNumberOfProducts((int)visibleFormwork.getWorth());
+                component.setUnit(product.getUnit());
+                component.setPriceComponent(pricePerSquare);
+                componentController.edit(component);
+            } catch (Exception ex) {
+                Logger.getLogger(Assembling_VisibleFormworkController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
         if (!ProjectViewController.isProjectOpened()) {
             abatementPercent.setProject(ProjectViewController.getOpenedProject());
             abatementArea.setProject(ProjectViewController.getOpenedProject());
             visibleFormwork.setProject(ProjectViewController.getOpenedProject());
             productCosts.setProject(ProjectViewController.getOpenedProject());
-            pricePerSquare.setProject(ProjectViewController.getOpenedProject());
             assemblingDuration.setProject(ProjectViewController.getOpenedProject());
             assemblingCosts.setProject(ProjectViewController.getOpenedProject());
             totalCosts.setProject(ProjectViewController.getOpenedProject());
             workerCosts.setProject(ProjectViewController.getOpenedProject());
 
-            worthController.create(pricePerSquare);
             worthController.create(abatementPercent);
             worthController.create(workerCosts);
             worthController.create(assemblingDuration);
@@ -185,7 +227,6 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
             worthController.create(totalCosts);
         } else {
             try {
-                worthController.edit(pricePerSquare);
                 worthController.edit(abatementPercent);
                 worthController.edit(workerCosts);
                 worthController.edit(assemblingDuration);
@@ -200,12 +241,36 @@ public class Assembling_VisibleFormworkController implements Initializable, Obse
     }
 
     private void calculateFormwork() {
+        //Verschnittsfläche
+        //Alte Formel-ID: VSS
+        abatementArea.setWorth(Project_ResultAreaController.getInstance().getLedge() * abatementPercent.getWorth()/ 100);
+        lb_AbatementArea.setText(abatementArea.worthFormatWithUnit());
 
+        //sicht. Schalung
+        //Alte Formel-ID: SS
+        visibleFormwork.setWorth(Project_ResultAreaController.getInstance().getLedge() + abatementArea.getWorth());
+        lb_VisibleFormwork.setText(UtilityFormat.getStringForLabel(visibleFormwork));
+
+        //Kosten Produkt
+        //Alte Formel-ID: KPSS
+        productCosts.setWorth(pricePerSquare * visibleFormwork.getWorth());
+        lb_ProductCosts.setText(UtilityFormat.getStringForLabel(productCosts));
+
+        //Kosten Montage
+        //Alte Formel-ID: KMSS
+        assemblingCosts.setWorth(workerCosts.getWorth() * assemblingDuration.getWorth());
+        lb_AssemblingCosts.setText(UtilityFormat.getStringForLabel(assemblingCosts));
+
+        //Gesamtkosten
+        //Alte Formel-ID: GKSS
+        totalCosts.setWorth(productCosts.getWorth() + assemblingCosts.getWorth());
+        lb_TotalCosts.setText(UtilityFormat.getStringForLabel(totalCosts));
     }
 
     @Override
     public void update(Observable o, Object arg) {
         double roofArea = Project_ResultAreaController.getInstance().getRoofArea();
         lb_RoofArea.setText(UtilityFormat.getStringForLabel(roofArea) + " m²");
+        calculateFormwork();
     }
 }
